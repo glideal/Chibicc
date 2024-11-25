d6e7547b1e739dc32146c9613f02f855f2a5018f
korekurainooventovakoni@gmail.com <glideal@babatLAPTOP>
2024-11-25 17:23:07 +0900

    introduce the concept of Node, */ operator and ()

--- main.c
+++ main.c
@@ -9,11 +9,11 @@ typedef enum{
     TK_RESERVED,
     TK_NUM,
     TK_EOF,
-} Tokenkind;
+} TokenKind;
 
 typedef struct Token Token;
 struct Token{
-    Tokenkind kind;
+    TokenKind kind;
     Token*next;
     int val;
     char*str;
@@ -76,7 +76,7 @@ bool at_eof(){
     return token->kind==TK_EOF;
 }
 
-Token*new_token(Tokenkind kind,Token*cur,char*str){
+Token*new_token(TokenKind kind,Token*cur,char*str){
     Token*tok=calloc(1,sizeof(Token));
     tok->kind=kind;
     tok->str=str;
@@ -96,7 +96,15 @@ Token*tokenize(){
             continue;
         }
 
-        if(*p=='+'||*p=='-'){
+        /*
+        本来は
+        strchr(const char*s,int c)
+        s=対象の文字列 : c=探す文字 (cをchar型にキャストしたもの)
+        であるが、ここでは
+        cがsにあるいずれかの文字でないかをチェックしている。
+        天才。
+        */
+        if(strchr("+-*/()",*p)){
             cur=new_token(TK_RESERVED,cur,p);
             p++;
             continue;
@@ -113,6 +121,128 @@ Token*tokenize(){
     return head.next;
 }
 
+//
+//parser
+//
+
+typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+}NodeKind;
+
+typedef struct Node Node;
+struct Node{
+    NodeKind kind;
+    Node*lhs;
+    Node*rhs;
+    int val;
+};
+
+Node*new_node(NodeKind kind){
+    Node*node=calloc(1,sizeof(Node));
+    node->kind=kind;
+    return node;
+}
+
+Node*new_binary(TokenKind kind,Node*lhs,Node*rhs){
+    Node*node=new_node(kind);
+    node->lhs=lhs;
+    node->rhs=rhs;
+    return node;
+}
+
+Node*new_num(int val){
+    Node*node=new_node(ND_NUM);
+    node->val=val;
+    return node;
+}
+
+Node*expr();
+Node*mul();
+Node*primary();
+
+//expr=mul("+" mul |"-" mul)*
+Node*expr(){
+    Node*node=mul();
+
+    for(;;){
+        if(consume('+')){
+            node=new_binary(ND_ADD,node,mul());
+        }else if(consume('-')){
+            node=new_binary(ND_SUB,node,mul());
+        }else{
+            return node;
+        }
+    }
+}
+
+//mul=primary("*" primary | "/" primary)*
+Node*mul(){
+    Node*node=primary();
+    for(;;){
+        if(consume('*')){
+            node=new_binary(ND_MUL,node,primary());
+        }else if(consume('/')){
+            node=new_binary(ND_DIV,node,primary());
+        }else{
+            return node;
+        }
+    }
+}
+
+//primary="(" expr ")" | num
+Node*primary(){
+    Node*node=calloc(1,sizeof(Node*));
+    if(consume('(')){
+        node=expr();
+        expect(')');
+    }else{
+        node=new_num(expect_number());
+    }
+    return node;
+}
+
+void gen(Node*node){
+    if(node->kind==ND_NUM){
+        printf("  push %d\n",node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch(node->kind){
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            /*
+            idiv (arg)
+            >>>(rdx+rax)(128bit) / (arg)(64bit) = rax ... rdx
+            ^raxに商、rdxに余りはidivのデフフォルト
+
+            cqo 
+            >>>rax(64bit)を(rax+rdx)(128bit)に引き延ばす
+            */
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
 
 int main(int argc, char **argv){
     if(argc!=2){
@@ -122,20 +252,15 @@ int main(int argc, char **argv){
 
     user_input=argv[1];
     token=tokenize();
+    Node*node=expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n",expect_number());
 
-    while(!at_eof()){
-        if(consume('+')){
-            printf("  add rax, %d\n",expect_number());
-            continue;
-        }
-        expect('-');
-        printf("  sub rax, %d\n",expect_number());
-    }
+    gen(node);
+
+    printf("  pop rax\n");
     printf("  ret\n");
     printf(".section .note.GNU-stack,\"\",@progbits\n");
 
