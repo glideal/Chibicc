9b343730aca080b5f88849fe3f0151dfe46283ed
korekurainooventovakoni@gmail.com <glideal@babatLAPTOP>
2024-11-25 13:25:44 +0900

    introduce + and -

--- main.c
+++ main.c
@@ -1,21 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-//test
+#include<ctype.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdbool.h>
 
 int main(int argc, char **argv){
     if(argc!=2){
         fprintf(stderr,"%s: invalid number of arguments\n", argv[0]);
         return 1;
     }
+    char*p=argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n",atoi(argv[1]));
-    printf("  ret\n");
+    printf("  mov rax, %ld\n",strtol(p,&p,10));
+
+    while(*p){
+        if(*p=='+'){
+            p++;
+            printf("  add rax, %ld\n",strtol(p,&p,10));
+            continue;
+        }
+        
+        if(*p=='-'){
+            p++;
+            printf("  sub rax, %ld\n",strtol(p,&p,10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected characte: '%c'\n",*p);
+        return 1;
+    }
     printf("  ret\n");
     printf(".section .note.GNU-stack,\"\",@progbits\n");
-
 
     return 0;
 }
