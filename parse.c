8e82b0dba655cba8e2e7091cea8509deef65079d
korekurainooventovakoni@gmail.com <glideal@babatLAPTOP>
2025-03-13 22:15:49 +0900

    support file-scope functions // static int function(){...}

--- parse.c
+++ parse.c
@@ -667,6 +667,7 @@ Function*function(){
     //construct a function object
     Function*fn=calloc(1,sizeof(Function));
     fn->name=name;
+    fn->is_static=ty->is_static;
     expect("(");
     fn->params=read_func_params();
     if(consume(";")){
